c89e0ee73a5a1a940c1e65ccc300dd2575657470
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-25 09:36:00 +0900

    ADD :: function Type

--- src/test.ts
+++ src/test.ts
@@ -4,3 +4,16 @@ const stringType: string = "Hello World";
 const booleanType: boolean = false;
 
 let arrayType: string[] = ["first", "second"];
+
+//function Parameter Type
+function func1(name: string) {
+  console.log(name);
+}
+
+//Argument of type 'number' is not assignable to parameter of type 'string'.
+//func1(51);
+
+//function Return Type
+function func2(): string {
+  return "";
+}
